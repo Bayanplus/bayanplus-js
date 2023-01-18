64bd97e4c8e94eaafec0f94883eca44bbb55a1d2
Salah <salahandstuff@gmail.com>
2023-01-18 13:55:14 +0300

    feat: `/demo` as an example of usage using `vite`

---
+++ demo/src/main.ts
@@ -0,0 +1,21 @@
+import bayanplus from 'bayanplus-js';
+
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const run = async () => {
+  bayanplus.event('This is custom event');
+  bayanplus.user.set({
+    name: 'Salah',
+    email: '',
+  });
+  console.log('before load, bayanplus.user.get(): ', bayanplus.user.get());
+  bayanplus.init({
+    projectId: 'xxxxxx',
+    trackLocalhost: true,
+    exclude: ['/exclude'],
+  });
+  await sleep(1000);
+  console.log('after load bayanplus.user.get(): ', bayanplus.user.get());
+};
+
+(window as any).run = run;
